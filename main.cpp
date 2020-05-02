09c1b0a8913024e3eaf4277c93efb34d9f258db7
Unicue <colin.yuen@hotmail.com>
2020-05-01 21:03:23 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,12 +1,20 @@
-//g++ main.cpp -o Output -lcurl 
+//g++ -Wall -Wextra -std=c++17 main.cpp -o Output -lcurl 
 #include <curl/curl.h>
 #include <cstring>
 #include <iostream>
 #include <fstream>
+#include <filesystem>
+#include <chrono>
+#include <iomanip>
 
 using namespace std;
 
-const string API_Key = "trnsl.1.1.20200422T024223Z.8ed8c69f7072557a.e285aeb7a16dd7816a09810c641e040ae835269b";
+string API_Key = "trnsl.1.1.20200422T024223Z.8ed8c69f7072557a.e285aeb7a16dd7816a09810c641e040ae835269b";
+filesystem::path msgoPath;
+filesystem::directory_entry latestLog;
+fstream file;
+
+void openLatestLog();
 
 static size_t writeCallback(void *data, size_t size, size_t nmemb, void* userp);
 void translate(string text);
@@ -19,10 +27,36 @@ int main(){
     //Either: Ask User to find MSGO / File search in program
     //Open Latest File
     //Go to end of the -> Loop until file is modified / updated -> Read Line (Print left of ":" & Translate Right side)
-    translate("さっきだれか撃ってるようにみえた");
+    //translate("さっきだれか撃ってるようにみえた");
+    openLatestLog();
+
+    string line;
+    
+    file.open(latestLog.path());
+    if(file.is_open()){
+        while(getline(file, line)){
+            line.length();
+            cout << line << "\n";
+        }
+        file.close();
+    }
+
     return 0;
 }
- 
+
+void openLatestLog(){
+    filesystem::path path = "Chat";
+    for(const auto& entry : filesystem::directory_iterator(path)){
+        latestLog = entry;
+    }
+
+    for(const auto& entry : filesystem::directory_iterator(path)){
+        if(entry.last_write_time() > filesystem::last_write_time(latestLog.path())){
+            latestLog = entry;
+        }
+    }
+}
+
 static size_t writeCallback(void *data, size_t size, size_t nmemb, void* userp){
     ((string*)userp)->append((char*)data, size * nmemb);
     return size * nmemb;
@@ -33,8 +67,8 @@ void translate(string text){
     CURLcode res;
 
     curl_global_init(CURL_GLOBAL_DEFAULT);
-
     curl = curl_easy_init();
+
     if(curl){
         string readBuffer;
         curl_easy_setopt(curl, CURLOPT_URL, getURL(text).c_str());
@@ -49,7 +83,7 @@ void translate(string text){
         curl_easy_cleanup(curl);
         cout << trimBuffer(readBuffer) << endl;
     }
-
+    
     curl_global_cleanup();
 }
 
@@ -65,20 +99,3 @@ string getURL(string text){
     + "&lang=ja-en"
     + "&[options=plain]";
 }
-
-/*
-    Get the whole line
-    Splice with :
-    Print on the left as normal, translate then print the right
-*/
-/*
-    fstream file;
-    seekg(ios::end);
-*/
-
-/*
-    Structure
-        - Set Up Curl
-        - Load Chat Log
-        - Start Loop
-*/
